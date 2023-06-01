e383ea1c762cf222ac7c837c9cb50c473d1cfb32
zmoodiedev <dev@zachmoodie.com>
2023-06-01 16:11:56 -0600

    Computer random choice function

--- js/main.js
+++ js/main.js
@@ -1,1 +1,27 @@
-alert("This works!");
+function getComputerChoice() {
+    let computerSelection = Math.floor(Math.random() * 3) + 1;
+
+    if (computerSelection === 1) {
+        return("Rock");
+    } else if (computerSelection === 2) {
+        return("Paper");
+    } else {
+        return("Scissors");
+    }
+}
+
+console.log(getComputerChoice());
+
+function playRound(playerSelection, computerSelection) {
+    
+}
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+
+console.log(playRound(playerSelection, computerSelection));
+
+/* SUDO:
+NEED TO RETURN RANDOM CHOICE. Either Rock, Paper, or Scissors
+
+*/
