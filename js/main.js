c5fcdb3c6a23dbe65be56504317bad03ffa58fd9
zmoodiedev <dev@zachmoodie.com>
2023-06-02 10:01:09 -0600

    Full game creation process start

--- js/main.js
+++ js/main.js
@@ -1,27 +1,100 @@
-function getComputerChoice() {
-    let computerSelection = Math.floor(Math.random() * 3) + 1;
+function game() {
 
-    if (computerSelection === 1) {
-        return("Rock");
-    } else if (computerSelection === 2) {
-        return("Paper");
-    } else {
-        return("Scissors");
+    let playerScore = 0;
+    let computerScore = 0;
+
+    console.log("Player Score: " + playerScore);
+    console.log("Computer Score: " + computerScore);
+
+    function playerWin() {
+        console.log("You win!")
+        playerScore += 1;
     }
-}
 
-console.log(getComputerChoice());
+    function computerWin() {
+        console.log("You Lose!")
+        computerScore += 1;
+    }
 
-function playRound(playerSelection, computerSelection) {
+    function tieGame() {
+        console.log("It's a tie!")
+    }
+
+
+    function playRound() {
+
+        let playerSelection = prompt("Rock, Paper, or Scissors?").toLowerCase();
+
+        console.log("Player chooses " + playerSelection + ".");
+
+        let computerSelection;
+
+        function getComputerChoice() {
+            
+            let randomNum = Math.floor((Math.random() * 3) + 1);
+        
+            if (randomNum === 1) {
+                computerSelection = "rock";
+            } else if (randomNum === 2) {
+                computerSelection = "paper";
+            } else {
+                computerSelection = "scissors";
+            }
+        
+            return(computerSelection);
+        }
+
+        console.log("Computer chooses " + getComputerChoice() + ".");
+
+        if (computerSelection === "rock") {
     
+            if (playerSelection === "rock") {
+               return(tieText);
+            } else if (playerSelection === "paper") {
+                return(winningText);
+            } else {
+                return(losingText);
+            }
+    
+        }
+        
+        if (computerSelection === "paper") {
+    
+            if (playerSelection === "rock") {
+                return(losingText);
+            } else if (playerSelection === "paper") {
+                return(tieText);
+            } else {
+                return(winningText);
+            }
+    
+        }
+        
+        if (computerSelection === "scissors") {
+            
+            if (playerSelection === "rock") {
+                return(winningText);
+            } else if (playerSelection === "paper") {
+                return(losingText);
+            } else {
+                return(tieText);
+            }
+        }
+
+    }
+
+    console.log(playRound());
+
 }
 
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
-
-console.log(playRound(playerSelection, computerSelection));
+game();
 
 /* SUDO:
-NEED TO RETURN RANDOM CHOICE. Either Rock, Paper, or Scissors
+Game needs to have 5 rounds.
+    Call the playRound function 5 times.
+    increase score of winner at end of each round
+Need to keep score of both players as games progress.
+    Need variable for each player score
 
+Report a winner at the end.
 */
